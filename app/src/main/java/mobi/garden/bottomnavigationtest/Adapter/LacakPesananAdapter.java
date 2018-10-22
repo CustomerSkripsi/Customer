8d83f552abcd659f3325b39cac0f5246c505d9b4
johniegorethai <35389396+johniegorethai@users.noreply.github.com>
2018-10-22 15:50:24 +0700

    history 2

---
+++ app/src/main/java/mobi/garden/bottomnavigationtest/Adapter/LacakPesananAdapter.java
@@ -0,0 +1,77 @@
+package mobi.garden.bottomnavigationtest.Adapter;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import mobi.garden.bottomnavigationtest.Activity.LacakPesananDetail;
+import mobi.garden.bottomnavigationtest.Model.Lacak;
+import mobi.garden.bottomnavigationtest.R;
+
+public class LacakPesananAdapter extends RecyclerView.Adapter<LacakPesananAdapter.LacakPesananViewHolder> {
+
+    private ArrayList<Lacak> mLacakList;
+
+    public LacakPesananAdapter(ArrayList<Lacak> LacakList){
+        mLacakList = LacakList;
+    }
+
+
+    @NonNull
+    @Override
+    public LacakPesananViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_list,parent, false);
+        LacakPesananViewHolder lacakPesananViewHolder = new LacakPesananViewHolder(view);
+        return lacakPesananViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LacakPesananAdapter.LacakPesananViewHolder holder, int position) {
+        final Lacak currentItem = mLacakList.get(position);
+
+        holder.orderID.setText(currentItem.getOrderID());
+        holder.tanggalTransaksi.setText(currentItem.getTanggal());
+        holder.outletName.setText(currentItem.getOutletName());
+        holder.statusOrder.setText(currentItem.getStatusOrder());
+
+        holder.transaksiDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), LacakPesananDetail.class);
+
+                i.putExtra("OrderID", currentItem.getOrderID());
+                i.putExtra("Tanggal", currentItem.getTanggal());
+                i.putExtra("OutletName", currentItem.getOutletName());
+                i.putExtra("StatusOrder", currentItem.getStatusOrder());
+
+                v.getContext().startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mLacakList.size();
+    }
+
+    public class LacakPesananViewHolder extends RecyclerView.ViewHolder{
+        TextView orderID, tanggalTransaksi, outletName, statusOrder;
+        ImageView transaksiDetail;
+
+        public LacakPesananViewHolder(View itemView) {
+            super(itemView);
+            orderID = itemView.findViewById(R.id.tv_OrderID);
+            tanggalTransaksi = itemView.findViewById(R.id.tv_tanggalTransaksi);
+            outletName = itemView.findViewById(R.id.tv_namaApotek);
+            statusOrder = itemView.findViewById(R.id.tv_StatusOrder);
+            transaksiDetail = itemView.findViewById(R.id.iv_transaksiDetail);
+        }
+    }
+}
---
+++ app/src/main/java/mobi/garden/bottomnavigationtest/Model/Lacak.java
@@ -0,0 +1,63 @@
+package mobi.garden.bottomnavigationtest.Model;
+
+public class Lacak {
+
+    private String orderID, tanggal, statusOrder, alamat, phone, outletName;
+    private int  totalHarga;
+
+    public String getOutletName() {
+        return outletName;
+    }
+
+    public void setOutletName(String outletName) {
+        this.outletName = outletName;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getStatusOrder() {
+        return statusOrder;
+    }
+
+    public void setStatusOrder(String statusOrder) {
+        this.statusOrder = statusOrder;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(int totalHarga) {
+        this.totalHarga = totalHarga;
+    }
+}
